ddac4312a3ce8e8f8f7d4d21438875c2a06931fd
KiddoV <viet.ho.03@gmail.com>
2025-07-24 20:54:51 -0500

    some updates...

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Enable static export
-  output: "export",
+  output: "export", // Replace "next export" cmd in [package.json]
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
   // Set base path. This is the slug of your GitHub repository.
-  basePath: "/web-portal",
+  basePath: "",
   // Disable server-based image optimization. Next.js does not support dynamic features with static exports.
   images: {
     unoptimized: true,
