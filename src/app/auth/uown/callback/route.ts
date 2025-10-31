8c654de695785058a7ae2a4dcbf6031cdaff52b2
Viet Ho <viet.ho.03@gmail.com>
2025-10-31 13:59:34 -0500

    fix(uown): second fix - handle auth callback

--- src/app/auth/uown/callback/route.ts
+++ src/app/auth/uown/callback/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/uown/server";
-import { env } from "process";
 
 ///
 export async function GET(request: Request) {
@@ -16,28 +15,22 @@ export async function POST(request: Request) {
 async function handleCallback(request: Request) {
   try {
     const { searchParams, origin } = new URL(request.url);
+    // Get the code from authenticator callback
     const code = searchParams.get("code");
-    // If "next" is in param, use it as the redirect URL
+    if (!code) throw new Error("Missing code");
+    // Next URL after login
     let next = searchParams.get("next") ?? "/";
     if (!next.startsWith("/")) next = "/";
-    if (!code) throw new Error("Missing code");
     //
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (error) throw error;
-    //
-    const forwardedHost = request.headers.get("x-forwarded-host"); // Original origin before load balancer
-    const isDev = env.NODE_ENV === "development";
-    if (isDev) {
-      // We can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-      return NextResponse.redirect(`${origin}${next}`);
-    } else if (forwardedHost) {
-      return NextResponse.redirect(`https://${forwardedHost}${next}`);
-    } else {
-      return NextResponse.redirect(`${origin}${next}`);
-    }
+    // Redirect to next page using absolute URL
+    return NextResponse.redirect(new URL(next, origin));
   } catch (err) {
-    console.error(err);
-    return NextResponse.redirect("/auth/uown/auth-code-error");
+    console.error("Auth callback error, ", err);
+    // Always use absolute URL in App Router
+    const { origin } = new URL(request.url);
+    return NextResponse.redirect(new URL("/auth/uown/auth-code-error", origin));
   }
 }
