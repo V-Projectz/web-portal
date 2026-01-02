17196176ac9fc8ddf28a20df987501ffd1a16300
Viet Ho <viet.ho.03@gmail.com>
2026-01-02 14:19:03 -0600

    fixed uown/api/get-issues

--- src/app/uown/api/get-issues/route.ts
+++ src/app/uown/api/get-issues/route.ts
@@ -3,20 +3,20 @@
  */
 
 import {App} from "@octokit/app";
+import { NextResponse } from "next/server";
 
 /// GET /uown/api/get-issues?userId=xxx
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const userId = searchParams.get("userId");
-  //
-  if (!userId) {
-    return Response.json({ error: "Missing userId" }, { status: 400 });
-  }
   // App key protection
   const appKey = req.headers.get("x-uown-app-key");
-  if (appKey !== process.env.UOWN_APP_KEY) {
-    return Response.json({ error: "Unauthorized" }, { status: 401});
-  }
+  if (appKey !== process.env.UOWN_APP_KEY) return NextResponse.json({ error: "Unauthorized" }, { status: 401});
+  //
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get("userId");
+  if (!userId) return NextResponse.json({ error: "Missing userId" }, { status: 400 });
+  // Paging params
+  const page = Number(searchParams.get("page") ?? "1");
+  const perPage = Math.min(Number(searchParams.get("perPage") ?? "20"), 100);
   //
   const app = new App({
     appId: process.env.UOWN_GITHUB_FEEDBACK_APP_ID!,
@@ -33,8 +33,10 @@ export async function GET(req: Request) {
       repo: process.env.UOWN_GITHUB_FEEDBACK_APP_REPO!,
       labels: `user:${userId}`,
       state: "all",
+      page: page,
+      per_page: perPage,
     }
   );
 
-  return Response.json(res.data);
+  return NextResponse.json(res.data);
 }
